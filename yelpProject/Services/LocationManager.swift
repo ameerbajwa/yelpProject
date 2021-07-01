3f47fd56dd550fe6b5a12345f22a2ea643c2d683
Ameer Bajwa <ameerbajwa@gmail.com>
2021-07-01 11:06:34 -0700

    Added new service file to call different yelp apis and a location manager to return user's location

--- yelpProject/Services/LocationManager.swift
+++ yelpProject/Services/LocationManager.swift
@@ -17,42 +17,6 @@ class LocationManager: NSObject, CLLocationManagerDelegate {
     
     var completion: ((CLLocation) -> Void)?
     
-//    func setupLocationManager() {
-//        manager.delegate = self
-//        manager.desiredAccuracy = kCLLocationAccuracyBest
-//        manager.startUpdatingLocation()
-//        manager.requestWhenInUseAuthorization()
-//    }
-//    
-//    func checkLocationServices() {
-//        if CLLocationManager.locationServicesEnabled() {
-//            setupLocationManager()
-//            checkLocationAuthorization()
-//        } else {
-//            // show alert letting the userknow they have to turn this on.
-//            print("locatino services off")
-//        }
-//    }
-//    
-//    func checkLocationAuthorization() {
-//        switch CLLocationManager.authorizationStatus() {
-//        case .authorizedWhenInUse:
-//            break
-//        case .denied:
-//            // show alert instructing them how to turn on permissions
-//            break
-//        case .notDetermined:
-//            manager.requestWhenInUseAuthorization()
-//        case .restricted:
-//            // show an alert letting them know what's up
-//            break
-//        case .authorizedAlways:
-//            break
-//        @unknown default:
-//            fatalError("")
-//        }
-//    }
-    
     public func getUserLocation(completion: @escaping ((CLLocation) -> Void)) {
         self.completion = completion
         manager.requestWhenInUseAuthorization()
@@ -66,6 +30,7 @@ class LocationManager: NSObject, CLLocationManagerDelegate {
         }
         completion?(location)
         manager.stopUpdatingLocation()
+        
         let coordinate = manager.location?.coordinate
         print("latitude: \(coordinate?.latitude)")
         print("longitude: \(coordinate?.longitude)")
