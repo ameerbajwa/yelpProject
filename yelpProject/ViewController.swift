5670b152ae9482a9f1f16913cad50d6738257f77
Ameer Bajwa <ameerbajwa@gmail.com>
2021-06-30 17:23:42 -0700

    first commit, created new xcode project

---
+++ yelpProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  yelpProject
+//
+//  Created by Ameer Bajwa on 6/30/21.
+//  Copyright © 2021 Ameer Bajwa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
